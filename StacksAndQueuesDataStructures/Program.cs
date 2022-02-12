d33d76f41c6c4f35a31ebb4a64f77a83c93c6bb1
RohanSampath <79681076+Rohan-dev-C@users.noreply.github.com>
2022-02-12 14:57:07 -0800

    Add files via upload

---
+++ StacksAndQueuesDataStructures/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace StacksAndQueuesDataStructures
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayStack<int> stak = new ArrayStack<int>();
+
+            stak.Push(5);
+            Console.WriteLine(stak.Pop().ToString());
+            stak.Push(1);
+            stak.Push(2);
+            stak.Push(3);
+            stak.Push(4);
+            stak.Push(9); 
+     
+            Console.WriteLine(stak.Pop().ToString());
+
+            stak.Push(5);
+            stak.Push(6);
+            stak.Push(7);
+            stak.Push(8);
+            stak.Pop();
+            Console.WriteLine(stak.Peek().ToString()); 
+
+
+            
+        }
+    }
+}
---
+++ StacksAndQueuesDataStructures/Stack.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StacksAndQueuesDataStructures
+{
+    class LinkedListStack<T>
+    {
+        public int Count { get; private set; }
+
+        LinkedList<T> data = new LinkedList<T>();
+
+        public LinkedListStack()
+        {
+
+        }
+
+        public void Push(T value)
+        {
+            data.AddLast(new LinkedListNode<T>(value));
+            Count++;
+        }
+
+        public T Pop()
+        {
+            LinkedListNode<T> temp = data.Last;
+            data.RemoveLast();
+            Count--;
+            return temp.Value;
+        }
+
+        public T Peek()
+        {
+            return data.Last.Value;
+        }
+
+        public void Clear()
+        {
+            Count = 0;
+            data.Clear();
+        }
+
+        public bool isEmpty()
+        {
+            return Count == 0;
+        }
+    }
+
+    class ArrayStack<T>
+    {
+        private T[] data;
+        public int Count { get; private set; }
+        public ArrayStack(int capacity = 4)
+        {
+            data = new T[capacity];
+        }
+        public void Push(T Value)
+        {
+            if (Count < data.Length)
+            {
+                data[Count] = Value;
+            }
+            else
+            {
+                Resize(Count + 1);
+                data[Count] = Value;
+            }
+            Count++;
+        }
+
+        public T Pop()
+        {
+            T temp = data[Count - 1];
+            data[Count - 1] = default;
+            Count--;
+            return temp;
+        }
+
+        public T Peek()
+        {
+            return data[Count - 1];
+        }
+
+        private void Resize(int newSize)
+        {
+            if (newSize == data.Length)
+            {
+                return;
+            }
+            T[] temp = new T[newSize];
+            for (int i = 0; i < Count; i++)
+            {
+                temp[i] = data[i];
+            }
+            data = temp;
+        }
+
+        public void Clear()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                data[i] = default;
+            }
+            Count = 0;
+        }
+
+        public bool isEmpty()
+        {
+            return Count == 0;
+        }
+    }
+}
